3e9a8b75d377343db062f9898057fd433034f68e
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-02-16 17:40:34 -0500

    reconfig alias

--- src/routes/charts/+page.svelte
+++ src/routes/charts/+page.svelte
@@ -1,6 +1,5 @@
-<script>
-
-  import Card from '$components/Card.svelte';
+<script>  
+import Card from '$components/Card.svelte';
   export let data; // `data` contains everything returned from `+layout.js`
   let breedsData = data.breedsData; // Extract breedsData
 
@@ -17,12 +16,4 @@
 
 <h1>SvelteKit Data App</h1>
 
-<ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-  {#each breedsData as breed}
-    <Card
-      title={breed.name}
-      subtitle={breed.temperament}
-      description={breed.description}>
-    </Card>
-  {/each}
-</ul>
+
