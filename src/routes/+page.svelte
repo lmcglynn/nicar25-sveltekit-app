1f4e2e2e2ae2715514674ba246e966baf027d53f
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-02-16 17:17:01 -0500

    tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,23 @@
 <script>
+
+  import Card from '../components/Card.svelte';
   export let data; // `data` contains everything returned from `+layout.js`
   let breedsData = data.breedsData; // Extract breedsData
 
-  import Card from '../components/Card.svelte';
+  $: weights = breedsData.reduce((acc, obj) => {
+  const value = obj.origin;
+  if (acc[value]) {
+    acc[value] += 1;
+  } else {
+    acc[value] = 1;
+  }
+  return acc;
+}, {});
+
+$: console.log(weights);
 </script>
 
-<h1>Homepage</h1>
+<h1>SvelteKit Data App</h1>
 
 <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
   {#each breedsData as breed}
