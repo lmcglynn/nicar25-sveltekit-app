cbab905a104f6a1c24905c09fd8053be27e26909
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-02-16 21:01:26 -0500

    fixing header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,15 @@
 <script>
 import "../app.css";
-
-import { AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<AppBar background="bg-black">
-  <div class="container flex justify-between items-center">
-    <a href="/" class="text-xl font-bold text-white	">First SvelteKit Data App</a>
+<div class="w-full flex justify-between items-center bg-black p-8">
+  <span class="text-xl font-bold text-white	">First SvelteKit Data App</span>
+  <div class="flex items-center">
+    <a href="/" class="text-white hover:text-gray-300">Home</a>
+    <a href="/charts" class="ml-4 text-white hover:text-gray-300">Charts</a>
   </div>
-</AppBar>
+</div>
+
+<div class="w-full flex bg-gray-300 px-8 py-2 justify-center">
+  Filters
+</div>
