ceb1edfc2b5df8ece1ad8c906b2d37349921c236
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-02-17 09:41:13 -0500

    remove skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,14 @@
 const { join } = require('path');
 
-// 1. Import the Skeleton plugin
-const { skeleton } = require('@skeletonlabs/tw-plugin');
-
 const config = {
-	// 2. Opt for dark mode to be handled via the class method
 	darkMode: 'class',
 	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+		'./src/**/*.{html,js,svelte,ts}'
 	],
 	theme: {
 		extend: {}
 	},
 	plugins: [
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton({
-			themes: { preset: [ "skeleton" ] }
-		}),
 		require('@tailwindcss/aspect-ratio')
 	]
 };
