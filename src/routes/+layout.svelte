b9b19d1b2b1ddb15be1cca28bfe4f05edfe2579f
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-02-17 19:28:02 -0500

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,3 +22,18 @@
 <main class="container flex justify-center w-full mx-auto mt-40 mb-8">
   <slot />
 </main>
+
+<footer class="flex justify-center w-full mx-auto mt-8 bg-slate-200 py-16">
+  <!-- three column grid, max 600 pixels wide, centered -->
+  <div class="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:max-w-2xl">
+    <div class="flex flex-col items-center">
+      <a href="https://schedules.ire.org/nicar-2025/">NICAR 25</a>
+    </div>
+    <div class="flex flex-col items-center">
+      <a href="https://github.com/tylermachado/nicar25-sveltekit-app">Github</a>
+    </div>
+    <div class="flex flex-col items-center">
+      <a href="https://thecatapi.com/">The Cat API</a>
+    </div>
+  </div>
+</footer>
